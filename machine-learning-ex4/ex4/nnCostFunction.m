af4b87469d7fc13bb584b4d6bef33abb61bf90bc
Konrad Listwan-Ciesielski <konrad.listwan@gmail.com>
2016-11-18 15:58:19 -0500

    Added Week 6 assignment. Fixed Week 5 solutions.

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -24,11 +24,11 @@ Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):en
 
 % Setup some useful variables
 m = size(X, 1);
-         
-% You need to return the following variables correctly 
+
 J = 0;
 Theta1_grad = zeros(size(Theta1));
 Theta2_grad = zeros(size(Theta2));
+
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: You should complete the code by working through the
@@ -62,30 +62,39 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+z2 = [ones(m, 1) X] * Theta1';
+a2 = sigmoid(z2);
+a2 = [ones(size(a2,1), 1) a2];
+hThetaX = sigmoid(a2 * Theta2');
+yVec = zeros(m, num_labels);
 
+for i = 1:m
+  yVec(i, y(i)) = 1;
+end
 
+J = 1/m * sum(sum(-1 * yVec .* log(hThetaX)-(1 - yVec) .* log(1 - hThetaX)));
+J = J + (sum(sum(Theta1(:, 2:end).^2)) + sum(sum(Theta2(: ,2:end).^2))) * (lambda/(2*m));
 
+for t = 1:m
+  a1 = [1; X(t,:)'];
 
+  z2 = Theta1 * a1;
+  a2 = [1; sigmoid(z2)];
+  z3 = Theta2 * a2;
+  a3 = sigmoid(z3);
 
+  yy = ([1:num_labels]==y(t))';
 
+  delta_3 = a3 - yy;
+  delta_2 = (Theta2' * delta_3) .* [1; sigmoidGradient(z2)];
+  delta_2 = delta_2(2: end);
+  Theta1_grad = Theta1_grad + delta_2 * a1';
+  Theta2_grad = Theta2_grad + delta_3 * a2';
+end
 
+Theta1_grad = (1/m) * Theta1_grad + (lambda/m) * [zeros(size(Theta1, 1), 1) Theta1(:, 2:end)];
+Theta2_grad = (1/m) * Theta2_grad + (lambda/m) * [zeros(size(Theta2, 1), 1) Theta2(:, 2:end)];
 
-
-
-
-
-
-
-
-
-
-
-% -------------------------------------------------------------
-
-% =========================================================================
-
-% Unroll gradients
 grad = [Theta1_grad(:) ; Theta2_grad(:)];
-
 
 end
