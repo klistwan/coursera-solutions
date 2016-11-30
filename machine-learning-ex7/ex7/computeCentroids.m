0cef0a85b97cd55463a03ed086d522d186ccc528
Konrad Listwan-Ciesielski <konrad.listwan@gmail.com>
2016-11-30 13:50:23 -0500

    Added Week 8 solutions.

--- machine-learning-ex7/ex7/computeCentroids.m
+++ machine-learning-ex7/ex7/computeCentroids.m
@@ -26,15 +26,12 @@ centroids = zeros(K, n);
 % Note: You can use a for-loop over the centroids to compute this.
 %
 
-
-
-
-
-
-
+for k = 1:K
+	index_of_k = find(idx == k);
+	centroids(k,:) = sum(X(index_of_k, :),1) / length(index_of_k);
+end
 
 % =============================================================
-
 
 end
 
